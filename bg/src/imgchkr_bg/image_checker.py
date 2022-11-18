2007ce2d6bb83354ab2dfeeb0fa8004b2d034df7
Peter Demin <peter.demin@toptal.com>
2022-11-18 16:41:38 -0500

    Add image size check

--- bg/src/imgchkr_bg/image_checker.py
+++ bg/src/imgchkr_bg/image_checker.py
@@ -1,11 +1,15 @@
+from typing import Dict, List
 from io import BytesIO
 from PIL import Image
 
 
 class ImageChecker:
+    _MAX_SIDE = 1000
 
-    def __call__(self, content: bytes) -> dict:
+    def __call__(self, content: bytes) -> Dict[str, List[str]]:
         """Checks image bytes by loading and trying to crop it.
+
+        Verifies that both width and height do not exceed 1000 px.
 
         Args:
             content: the image file contents as bytes.
@@ -13,10 +17,18 @@ class ImageChecker:
         Returns:
             Errors dict (empty if no error occured).
         """
+        errors = []
         with BytesIO(content) as buffer:
             try:
                 with Image.open(buffer) as image:
+                    width, height = image.size
+                    if width > self._MAX_SIDE:
+                        errors.append(f'Image width exceeds maximum ({width}/{self._MAX_SIDE})')
+                    if height > self._MAX_SIDE:
+                        errors.append(f'Image height exceeds maximum ({height}/{self._MAX_SIDE})')
                     image.crop((0, 0, 1, 1))
-                return {}
             except OSError as exc:
-                return {'image': str(exc)}
+                errors.append(str(exc))
+        if errors:
+            return {'image': errors}
+        return {}
--- bg/src/imgchkr_bg/test_image_checker.py
+++ bg/src/imgchkr_bg/test_image_checker.py
@@ -17,7 +17,16 @@ class ImageCheckerTestCase(unittest.TestCase):
 
     def test_corrupted_jpeg_fails(self) -> None:
         result = self._image_checker(self._read_jpeg('corrupted'))
-        assert result == {'image': 'broken data stream when reading image file'}
+        assert result == {'image': ['broken data stream when reading image file']}
+
+    def test_big_jpeg_fails(self) -> None:
+        result = self._image_checker(self._read_jpeg('big'))
+        assert result == {
+            'image': [
+                'Image width exceeds maximum (1800/1000)',
+                'Image height exceeds maximum (1200/1000)',
+            ]
+        }
 
     def _read_jpeg(self, label: str) -> bytes:
         with open(os.path.join(_TESTDATA, f'{label}.jpg'), 'rb') as fobj:
